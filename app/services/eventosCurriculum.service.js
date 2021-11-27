87dfaca1359fdbfa24d9fb72587bee5b618de43d
Jones Cesar <jonescnoel@gmail.com>
2021-11-27 19:02:26 -0300

    att

--- app/services/eventosCurriculum.service.js
+++ app/services/eventosCurriculum.service.js
@@ -4,6 +4,7 @@ exports.EventosCurriculum_create = async (data) => {
   try {
     const { codigo, nome, evento } = data;
     return await eventos_curriculum.create({ codigo, nome, evento });
+    //
   } catch (err) {
     console.warn("ERRO NovoCurriculum_create: " + err);
     return { erro: true, dev: "ERRO NovoCurriculum_create: ", message: err };
