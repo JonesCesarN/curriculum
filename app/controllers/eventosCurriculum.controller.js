5d7390ce5531d27e73830089262dd7ea4b3d5aa4
Jones Cesar <jonescnoel@gmail.com>
2021-12-02 12:06:13 -0300

    att

--- app/controllers/eventosCurriculum.controller.js
+++ app/controllers/eventosCurriculum.controller.js
@@ -11,19 +11,12 @@ exports.eventosCurriculum_search = async (req, res) => {
 exports.eventosCurriculum_getDay = async (req, res) => {
   let date = moment(new Date()).format("DD-MM-YYYY");
   const result = await EventosCurriculum_getDay(date);
-  let last6eventCurriculos = result.slice(result.length - 6);
-  const curriculosNovos = result.filter((i) => i.evento == "novo");
-  const curriculosEdit = result.filter((i) => i.evento == "editado");
-  let qtdsNews = curriculosNovos.length;
-  let qtdsEdit = curriculosEdit.length;
-  let valorNews = qtdsNews * 5.0;
-  let valorEdit = qtdsEdit * 2.0;
-
-  // console.log({ qtdsNews, qtdsEdit });
-
-  // console.log(curriculosNovos);
-  // console.log(curriculosEdit);
-  res.json({ curriculosNovos, curriculosEdit, result });
+  if (result.length > 5) {
+    let last6eventCurriculos = result.slice(result.length - 6);
+    res.json({ result: last6eventCurriculos });
+  } else {
+    res.json({ result });
+  }
 };
 
 exports.eventosCurriculum_update = async (req, res) => {
